a261e1adb7a7301a63c658e2c35c865d46d4a295
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2022-01-24 10:58:29 +0530

    Add files via upload

---
+++ DIT_CAF_Batch_Eclipse/Java/src/oops/Student.java
@@ -0,0 +1,41 @@
+package com.dit.java.oops;
+
+public class Student {
+	
+	//Class variables, instance variables, Data members
+	
+	//private member variables
+	//Private: Data Security - Data Hiding
+	private int rollno; 
+	private String name;
+	private String phone;
+	private String course;
+	private double fees;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student ram = new Student(); // ram: reference variable
+		
+//		System.out.println("Roll no: "+ram.rollno);
+//		System.out.println("Name is: "+ram.name);
+//		System.out.println("Course is: "+ram.course);
+//		System.out.println("Phone is: "+ram.phone);
+//		System.out.println("Fees is: "+ram.fees);
+		
+//		ram.rollno = -1001;
+//		ram.name = "Ram Kumar";
+//		ram.phone = "7823687786";
+//		ram.course = "MCA";
+//		ram.fees = 10000.0;
+		
+//		System.out.println("****************************************************");
+//		
+//		System.out.println("Roll no: "+ram.rollno);
+//		System.out.println("Name is: "+ram.name);
+//		System.out.println("Course is: "+ram.course);
+//		System.out.println("Phone is: "+ram.phone);
+//		System.out.println("Fees is: "+ram.fees);
+	}
+
+}
